596c394c5a8a925b9f6ba6c3cede5a6e1e83676b
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-09-25 19:26:34 -0400

    Array in v-for

--- src/components/TypesPlans.vue
+++ src/components/TypesPlans.vue
@@ -9,8 +9,22 @@
                     icon: "clock",
                     title: "Team training",
                     subtitle: "Find a partner",
-                    discription: "Vestibulum, curabitur eu sem nibh ultrices sit nulla adipiscing. Nisl sit fames amet senectus eget sed duis vehicula. tristique",
-                    link:"Find a partner"
+                    description: "Vestibulum, curabitur eu sem nibh ultrices sit nulla adipiscing. Nisl sit fames amet senectus eget sed duis vehicula. tristique",
+                    link:"Find a partner "
+                },
+                {
+                    icon: "",
+                    title: "Crossfit workout",
+                    subtitle: "Push your limits",
+                    description: "Vestibulum, curabitur eu sem nibh ultrices sit nulla adipiscing. Nisl sit fames amet senectus eget sed duis vehicula. tristique",
+                    link:"Learn about crossfit "
+                },
+                {
+                    icon: "clock",
+                    title: "Weigth loss plan",
+                    subtitle: "Shed those pounds",
+                    description: "At laoreet senectus volutpat diam vel sed amet pellentesque. Lobortis ut aliquam risus purus. A tellus ut etiam.",
+                    link:"Schedule a workout "
                 },
 ]
         }
@@ -19,14 +33,14 @@
 </script>
 
 <template>
-    <section v-for="item in info">   
+    <section v-for="(item, i) in info" :key="i">   
         <a href="https://themeforest.net/item/avada-responsive-multipurpose-theme/2833226?irgwc=1&clickid=RTpRi5yN2xyKTIqQnV04UVzBUkCyM5U5S2udUA0&iradid=275988&irpid=1292887&iradtype=ONLINE_TRACKING_LINK&irmptype=mediapartner&mp_value1=&utm_campaign=af_impact_radius_1292887&utm_medium=affiliate&utm_source=impact_radius" id="card" class="container">
-            <i id="icon" class="fa-solid fa-{{info.icon}}"></i>
-            <h3>{{ info.title}}</h3>
-            <h5>{{ info.subtitle}}Subtitle</h5>
-            <p>{{ info.description}}Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dolore dolorum optio sed consequatur sint excepturi quo eum minus, est, praesentium, nesciunt consequuntur iusto inventore assumenda corporis veritatis fuga amet.</p>
+            <i id="icon" class="fa-regula fa-clock"></i>
+            <h3>{{ info[i].title}}</h3>
+            <h5>{{ info[i].subtitle}}</h5>
+            <p>{{ info[i].description}}</p>
             <div>
-                <span>{{ info.description}}link sito </span>
+                <span>{{ info[i].link}}</span>
                 <i class="fa-solid fa-chevron-right "></i>
             </div>
         </a> 
@@ -40,6 +54,7 @@ section {
     background-color: black;
     color: white;
     padding: 30px;
+    
 
     #card{
         width: 30%;
