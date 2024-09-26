c4ea89c1adb3b2351f596ca47dff307787df3c36
Marcap00 <cappellatomarco@yahoo.com>
2024-09-26 13:11:25 +0200

    completed styles for newsletter section

--- src/components/main/MainNewsletter.vue
+++ src/components/main/MainNewsletter.vue
@@ -4,7 +4,12 @@
 export default {
     data() {
         return {
-
+            inputUserEmail: ''
+        }
+    },
+    methods: {
+        logEmail(email) {
+            console.log(email);
         }
     },
     components: {
@@ -16,13 +21,73 @@ export default {
 
 <template>
     <section id="newsletter">
-        <h3>Join our mailing list today</h3>
-        <h2>Insider offers & flash sales in your inbox every week.</h2>
-        <input type="email" placeholder="Insert your email ...*">
-        <!-- BaseButton -->
-        <p>Curabitur non nulla sit amet nisl tempus convallis quis ac lectus dolor sit amet, consectetur adipiscing elit
-            sed porttitor lectus.</p>
+        <div class="newsletter-body">
+            <h3>Join our mailing list today</h3>
+            <h2>Insider offers & flash sales in your inbox every week.</h2>
+            <div class="input-email">
+                <input @keyup.enter="logEmail(inputUserEmail)" v-model="inputUserEmail" type="email"
+                    placeholder="Insert your email ...*">
+            </div>
+            <!-- BaseButton -->
+            <p>Curabitur non nulla sit amet nisl tempus convallis quis ac lectus dolor sit amet, consectetur
+                adipiscing
+                elit
+                sed porttitor lectus.</p>
+        </div>
     </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+#newsletter {
+
+    background: linear-gradient(90deg, #000 47%, transparent 76%), url('../../assets/img/banner1-2x.jpg');
+    background-size: cover;
+    background-position: bottom 0 right 0;
+    padding: 5rem;
+
+
+    .newsletter-body {
+        max-width: 60%;
+
+        >* {
+            margin-bottom: 2rem;
+        }
+
+        h3 {
+            color: rgb(255, 65, 75);
+        }
+
+        h2 {
+            font-size: 58px;
+            color: #fff;
+        }
+
+        input,
+        input::placeholder {
+            font-size: 18px;
+        }
+
+        input {
+            width: 60%;
+            line-height: 55px;
+            padding: 0 1rem;
+            border-radius: 30px;
+
+            &:focus {
+                outline: none;
+            }
+
+            &::placeholder {
+                color: rgb(152, 147, 159);
+            }
+        }
+
+        p {
+            font-size: 23px;
+            color: rgb(152, 147, 159);
+            margin-bottom: 0;
+        }
+    }
+
+}
+</style>
